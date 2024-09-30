73841752834a0b8dd8bfbfdc3b6fc4b457e1f22a
fteran04 <110037380+fteran04@users.noreply.github.com>
2024-09-29 20:35:43 -0500

    Update Babylon.js
    Versión de la escena con mas mallas y uso de texturas

--- Babylon.js
+++ Babylon.js
@@ -4,7 +4,9 @@ var createScene = function () {
     var camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 4, 40, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
 
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(10, 10, 10), scene);
+
+    // Cargar el piso
     BABYLON.SceneLoader.ImportMesh(
         "", 
         "https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/", 
@@ -13,33 +15,128 @@ var createScene = function () {
         function (newMeshes) {
             newMeshes.forEach(mesh => {
                 mesh.position = BABYLON.Vector3.Zero(); 
-                console.log("Piso cargado correctamente.");
-                mesh.scaling = new BABYLON.Vector3(2,2,2);
+                mesh.scaling = new BABYLON.Vector3(2, 2, 2);
                 mesh.position = new BABYLON.Vector3(0, 3.2, 0);
+
+                // textura
+                var groundMaterial = new BABYLON.StandardMaterial("groundMat", scene);
+                groundMaterial.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/ground_texture.webp", scene);
+                mesh.material = groundMaterial;
+
+                console.log("Piso cargado y textura aplicada.");
             });
         },
         null,
         null
     );
 
-
+    // Cargar la pared
     BABYLON.SceneLoader.ImportMesh(
         "", 
         "https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/", 
-        "Door.obj", 
+        "wall.obj", 
         scene,
         function (newMeshes) {
             newMeshes.forEach(mesh => {
-                mesh.position = new BABYLON.Vector3(5, 0, -8); 
-                mesh.rotation = new BABYLON.Vector3(0, Math.PI /4, 0);
-                mesh.scaling = new BABYLON.Vector3(3, 3, 3);
-                console.log("Puerta cargada correctamente.");
+                mesh.position = new BABYLON.Vector3(5, 0, -23); 
+                mesh.rotation = new BABYLON.Vector3(0, -0.5, 0);
+                mesh.scaling = new BABYLON.Vector3(10, 10, 10);
+
+                // textura
+                var wallMaterial = new BABYLON.StandardMaterial("wallMat", scene);
+                wallMaterial.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/albedo.jpg", scene); 
+                mesh.material = wallMaterial;
+
+                console.log("pared cargada y textura aplicada.");
             });
         },
         null,
         null
     );
 
+
+    // Cargar la puerta
+    BABYLON.SceneLoader.ImportMesh(
+        "", 
+        "https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/", 
+        "door.obj", 
+        scene,
+        function (newMeshes) {
+            newMeshes.forEach(mesh => {
+                mesh.position = new BABYLON.Vector3(-120, 40, -175);
+                mesh.rotation = new BABYLON.Vector3(0, -0.5, 0);
+                mesh.scaling = new BABYLON.Vector3(10, 10, 10);
+
+                // textura
+                var doorMaterial = new BABYLON.StandardMaterial("doorMat", scene);
+                doorMaterial.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/T_Door_D.png", scene); 
+                mesh.material = doorMaterial;
+
+                console.log("puerta cargada y textura aplicada.");
+            });
+        },
+        null,
+        null
+    );
+
+
+
+    // Cargar arbol
+    BABYLON.SceneLoader.ImportMesh(
+        "", 
+        "https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/", 
+        "pine.obj", 
+        scene,
+        function (newMeshes) {
+            newMeshes.forEach(mesh => {
+                mesh.position = new BABYLON.Vector3(5, 1, 15);
+                mesh.rotation = new BABYLON.Vector3(0, 0.1, 0);
+                mesh.scaling = new BABYLON.Vector3(2, 2, 2);
+
+                // textura
+                var pineMaterial = new BABYLON.StandardMaterial("pineMat", scene);
+                pineMaterial.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/Pine_BaseColor.jpeg", scene);
+                mesh.material = pineMaterial;
+
+                console.log("puerta cargada y textura aplicada.");
+            });
+        },
+        null,
+        null
+    );
+
+
+    // Cargar mago
+    BABYLON.SceneLoader.ImportMesh(
+        "", 
+        "https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/", 
+        "gandalf.obj", 
+        scene,
+        function (newMeshes) {
+            newMeshes.forEach(mesh => {
+                mesh.position = new BABYLON.Vector3(-10, 1, -5);
+                mesh.rotation = new BABYLON.Vector3(0, -5, 0);
+                mesh.scaling = new BABYLON.Vector3(16, 16, 16);
+
+                // textura
+                var mageMaterial = new BABYLON.StandardMaterial("mageMat", scene);
+                mageMaterial.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/gray_texture.jpg", scene); 
+                mesh.material = mageMaterial;
+
+                console.log("puerta cargada y textura aplicada.");
+            });
+        },
+        null,
+        null
+    );
+
+
+
+
+
+
+
+    // Cargar la carretilla
     BABYLON.SceneLoader.ImportMesh(
         "", 
         "https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/", 
@@ -47,16 +144,31 @@ var createScene = function () {
         scene,
         function (newMeshes) {
             newMeshes.forEach(mesh => {
-                mesh.position = new BABYLON.Vector3(0, 3, 10); 
-                console.log("Carretilla cargada correctamente.");
-                
-                // Crear un cubo sobre la carretilla
-                var cube = BABYLON.MeshBuilder.CreateBox("cube", { size: 2 }, scene);
-                cube.position = new BABYLON.Vector3(0, 8, 10); 
-                var cube = BABYLON.MeshBuilder.CreateBox("cube", { size: 2 }, scene);
-                cube.position = new BABYLON.Vector3(2.4, 8, 10); 
-                var cube = BABYLON.MeshBuilder.CreateBox("cube", { size: 2 }, scene);
-                cube.position = new BABYLON.Vector3(2, 8, 13); 
+                mesh.position = new BABYLON.Vector3(0, 3, 10);
+
+                // textura
+                var cartMaterial = new BABYLON.StandardMaterial("cartMat", scene);
+                cartMaterial.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/textura_carretilla.jpg", scene); // Cambia la ruta a tu textura
+                mesh.material = cartMaterial;
+
+                console.log("Carretilla cargada y textura aplicada.");
+
+                // Crear cubos encima de la carretilla
+                var cube1 = BABYLON.MeshBuilder.CreateBox("cube1", { size: 2 }, scene);
+                cube1.position = new BABYLON.Vector3(0, 8, 10);
+
+                var cube2 = BABYLON.MeshBuilder.CreateBox("cube2", { size: 2 }, scene);
+                cube2.position = new BABYLON.Vector3(2.4, 8, 10);
+
+                var cube3 = BABYLON.MeshBuilder.CreateBox("cube3", { size: 2 }, scene);
+                cube3.position = new BABYLON.Vector3(2, 8, 13);
+
+                // textura
+                var cubeMaterial = new BABYLON.StandardMaterial("cubeMat", scene);
+                cubeMaterial.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/kprdes/Compu_Grafica/main/box_texture.jpg", scene); // Cambia la ruta a tu textura
+                cube1.material = cubeMaterial;
+                cube2.material = cubeMaterial;
+                cube3.material = cubeMaterial;
             });
         },
         null,
@@ -65,22 +177,3 @@ var createScene = function () {
 
     return scene;
 };
-
-
-var canvas = document.getElementById("renderCanvas");
-
-
-var engine = new BABYLON.Engine(canvas, true);
-
-
-var scene = createScene();
-
-
-engine.runRenderLoop(function () {
-    scene.render();
-});
-
-
-window.addEventListener("resize", function () {
-    engine.resize();
-});
